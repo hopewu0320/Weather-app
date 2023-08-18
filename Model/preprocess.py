aabdb1f078d55a5e9a76b7474c76801fcb93a2f1
hopewu0320 <gccbs01596@gmail.com>
2023-08-19 03:23:33 +0800

    csv OK

--- Model/preprocess.py
+++ Model/preprocess.py
@@ -1,93 +1,78 @@
-#Hint
-#1.Encode 非數值的feature
-#2.Fill median value to Misiing value
-#3.處理Data imbalance問題 eg SMOTE
-#4.正規化,feature selection
-#處理流程:合併train,test ->Encode non numeric value -> fill Missing value ->(feature selection)-> Normalization -> train_test_split
-#https://blog.csdn.net/m0_37725003/article/details/81095555
-#https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.VotingClassifier.html
-#classification最後 feature selection(講義倒數)
-#補缺失值使用 同個class的median(X 不可行) 
-#先使用Ensemble方法 不行再用feature selection
-
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
-from collections import Counter
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 from sklearn.impute import KNNImputer
-from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import joblib
+from sklearn.metrics import accuracy_score, f1_score
 
 df_train = pd.read_csv('train.csv') #35156
 df_test = pd.read_csv('test.csv') #34844
 
 df_test['Weather'] = np.zeros(len(df_test))
-train_end_idx = len(df_train)
+train_end_idx = len(df_train) #35156
+
+
 df = pd.concat([df_train,df_test],ignore_index=True)
-
-
-
-#NightWindDir DayWindDir WindDir Loc Date
-#drop Loc,Date
 df = df.drop(columns=['Date','Loc','NightWindDir','DayWindDir','WindDir']) 
-df0=df[df['Weather']==0]
-df1=df[df['Weather']==1]
-
-#針對非數值feature處理
-#1.把nan去除後transform 算出median,2.transform整個feature並且把Nan值改成median 3.寫回df
-def Non_Numeric_transform(feature):
-    le = LabelEncoder()
-    le.fit(df[feature])
-    feature_Delete_NAN = le.transform(df[feature].dropna()) 
-    print(feature_Delete_NAN[:20])
-    feature_transform = le.transform(df[feature]) 
-    #print("修改前:{}".format(feature_transform[:10]))
-    for idx,nan in enumerate(pd.isna(df[feature])): #把nan的idx記錄下來
-        if(nan==False):
-            feature_transform[idx] = feature_transform[idx]
-    #print("修改後:{}".format(feature_transform[:10]))
-    df[feature] = feature_transform
-    #print('{}有NAN? : {}'.format(feature,df[feature].isnull().values.any()))
-
-Non_numeric_list = ['NightWindDir','DayWindDir','WindDir']
-Numeric_list = [c for c in df.columns if c not in Non_numeric_list]
-
-
-
-
 X_train, X_val, y_train, y_val = train_test_split(
     df.drop(columns = ['Weather']).values[:train_end_idx, :],
     df['Weather'].values[:train_end_idx], test_size=0.5,shuffle=True)
 
-X_test = df.drop(columns = ['Weather']).values[train_end_idx:, :]
+#1:1:2
+#train:0~17577 #val:17578~35155
 
-
-
+X_val = df_train[17578:35156]
+X_val = X_val.reset_index()
+y_val = X_val[['Weather']]
+X_Loc = X_val[['Loc']]
+X_val = X_val.drop(columns=['Date','Loc','NightWindDir','DayWindDir','WindDir','Weather','index']) 
 
 imputer = KNNImputer(n_neighbors=3)
-print("KNNImputer train start")
-X_train = imputer.fit_transform(X_train)
-print("KNNImputer val start")
+
 X_val = imputer.fit_transform(X_val)
-print("KNNImputer test start")
-X_test = imputer.fit_transform(X_test)
-print("KNNImputer Finish")
 
-sm = SMOTE( random_state=10) #10:0.33
-X_train, y_train = sm.fit_resample(X_train, y_train)
+real_data = pd.DataFrame(X_val,columns=["TempLow","TempHigh","Steaming","Sun","WindSpeed","DayWindSpeed","NightWindSpeed","DayHumidity","NightHumidity","DayPressure","NightPressure","DayCloud","NightCloud","DayTemp","NightTemp","RISK_MM"])
 
-
+print("Transforming")
 scaler = MinMaxScaler().fit(X_train)
 X_train = scaler.transform(X_train)
 X_val = scaler.transform(X_val)
-X_test = scaler.transform(X_test)
+print("Transform finish")
+X_val = pd.DataFrame(X_val, columns=["TempLow","TempHigh","Steaming","Sun","WindSpeed","DayWindSpeed","NightWindSpeed","DayHumidity","NightHumidity","DayPressure","NightPressure","DayCloud","NightCloud","DayTemp","NightTemp","RISK_MM"])
 
 
-np.save('X_train.npy',X_train)
-np.save('X_val.npy',X_val)
-np.save('y_train.npy',y_train)
-np.save('y_val.npy',y_val)
-np.save('X_test.npy',X_test)
+X_val['Loc'] = X_Loc
+X_val['Weather'] = y_val
+X_val = X_val.dropna()
+Rainy = X_val[X_val['Weather']==0].sample(40)
+Sunny = X_val[X_val['Weather']==1].sample(40)
+data = pd.concat([Rainy,Sunny])
+sample_Loc = data[['Loc']]
+sample_Weather = data[['Weather']]
+data = data.drop(columns=['Loc','Weather'])
+
+
+model = joblib.load('Weather_model')
+y_pred_decision = model.predict(data)
+y_pred_decision
+
+#real_data把跟data重複的索引取出
+result_df = real_data.loc[data.index]
+
+
+
+result_df['Loc'] = sample_Loc
+result_df['Weather'] = sample_Weather
+result_df['Guess'] = y_pred_decision
+
+#predict
+y_pred_decision = model.predict(data)
+
+Y_val = result_df['Weather'].to_numpy()
+print('Accuracy: %f' % accuracy_score(Y_val, y_pred_decision))
+print('f1-score: %f' % f1_score(Y_val, y_pred_decision))
+
+result_df.to_csv('result_df.csv',  index_label = 'Id')
