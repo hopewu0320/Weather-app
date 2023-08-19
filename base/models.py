a42a33119a611d8928f8c86832e35543b3a1a115
hopewu0320 <gccbs01596@gmail.com>
2023-08-19 22:34:13 +0800

    database update OK

--- base/models.py
+++ base/models.py
@@ -3,4 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Weather(models.Model):
-    temperature = models.FloatField()
+    Loc = models.CharField(max_length=200,null=True)
+    NightTemp = models.FloatField(null=True)
+    DayTemp = models.FloatField(null=True)
+    DayHumidity = models.FloatField(null=True)
+    NightHumidity = models.FloatField(null=True)
+    WindSpeed = models.FloatField(null=True)
+    real_weather = models.IntegerField(null=True)
+    guess_weather = models.IntegerField(null=True)
+    def __str__(self):
+        return "Loc: {} NightTemp: {} DayTemp: {}".format(self.Loc,self.NightTemp,self.DayTemp)
--- base/views.py
+++ base/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from .models import Weather
 import requests
+import pandas as pd
 # Create your views here.
-
+PATH = './Model/result_df.csv'
 def get_data(request):
-    
+    result_df = pd.read_csv(PATH) #35156
+    for index,row in result_df.iterrows():
+        if not Weather.objects.filter(Loc=row['Loc'],
+        NightTemp=row['NightTemp'],
+        DayTemp=row['DayTemp']).exists():
+            Weather.objects.create(
+                Loc = row['Loc'],
+                NightTemp = row['NightTemp'],
+                DayTemp = row['DayTemp'],
+                NightHumidity = row['NightHumidity'],
+                DayHumidity = row['DayHumidity'],
+                WindSpeed = row['WindSpeed'],
+                real_weather = row['Weather'],
+                guess_weather = row['Guess'],
+            )
     context = {}
     return render(request,'base/index.html',context)
