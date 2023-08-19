a2e59bda7c3a09a4ae1832ae75cf3814e21494d3
hopewu0320 <gccbs01596@gmail.com>
2023-08-20 01:44:41 +0800

    JsonResponse OK

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+#把前端初始網址放在path中
 urlpatterns = [
     path("", views.get_data,name='get_data'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from .models import Weather
 import requests
 import pandas as pd
+from django.http import JsonResponse
+import json
 # Create your views here.
+#前端重新整理 把df存入資料庫
 PATH = './Model/result_df.csv'
 def get_data(request):
     result_df = pd.read_csv(PATH) #35156
@@ -20,5 +23,15 @@ def get_data(request):
                 real_weather = row['Weather'],
                 guess_weather = row['Guess'],
             )
+    
+    json_list = []
+    for index,row in result_df.iterrows():
+        row2dict = dict(row)
+        json_list.append(row2dict)
+    
+    with open('sample.json', 'w') as f:
+        json.dump(json_list, f)
+
     context = {}
-    return render(request,'base/index.html',context)
+    
+    return JsonResponse(json_list,safe=False)  #should be dictionary or list or dictionary of list
