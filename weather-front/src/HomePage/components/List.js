019eeef4985966c8dbff5b6508ddfa94818dee3b
hopewu0320 <gccbs01596@gmail.com>
2023-08-26 04:12:02 +0800

    OX correction

--- weather-front/src/HomePage/components/List.js
+++ weather-front/src/HomePage/components/List.js
@@ -1,5 +1,7 @@
 import "../index.css"
 import WeatherData from "./WeatherData"
+import Sunny from '../../image/Sunny.jpg'
+import Rainy from '../../image/Rainy.jpg'
 import {useEffect,useState} from "react"
 function refresh(){
     alert('Hello')
@@ -9,7 +11,7 @@ onClick後 會去後端拉資料 顯示在前端(跳頁) onChange
 */
 
 
-const List = ({data,setSelectedItem})=>{
+const List = ({data,setSelectedItem,selectedItem})=>{
     // const users = [
     //     {name:"Pedro",age:21},
     //     {name:"Hope",age:23}
@@ -26,21 +28,41 @@ const List = ({data,setSelectedItem})=>{
         setSelectedItem(selectedData);
     }
     
-    return <div className="list">
-        <select  onChange={PredictWeather}>
+    
+    return <div className="ListData">
+        <select  onChange={PredictWeather} className="list">
             {data && data.map((item)=>{
                 return(
                     <option>
-                        <div >
-                            <p >
-                                {item.Loc} DayTemp:{item.DayTemp.toFixed(2)} NightTemp:{item.DayTemp.toFixed(2)} WindSpeed:{item.WindSpeed.toFixed(2)} DayHumidity:{item.DayHumidity.toFixed(2)} NightHumidity:{item.NightHumidity.toFixed(2)}
-                            </p>
+                        <div >                      
+                            {item.Loc} DayTemp:{item.DayTemp.toFixed(2)} NightTemp:{item.DayTemp.toFixed(2)} WindSpeed:{item.WindSpeed.toFixed(2)} DayHumidity:{item.DayHumidity.toFixed(2)} NightHumidity:{item.NightHumidity.toFixed(2)}                  
                         </div>
                     </option>
                 )
             })}
         </select>
-        
+        {selectedItem.Guess==selectedItem.Weather ?
+        <div className="ShowCorrect">
+            <div className="OX">
+                <p style={{color: 'green'}}>
+                    O
+                </p>
+            </div>
+            <div className="correction">
+                1234
+            </div>
+        </div>:
+        <div className="ShowCorrect">
+            <div className="OX">
+                <p style={{color: 'red'}}>
+                    X  {selectedItem.Guess ? "Sunny" : "Rainy"} => {selectedItem.Guess ? "Rainy" : "Sunny"}
+                </p>
+            </div>
+            <div className="correction">
+                1234
+            </div>
+        </div>
+        }
     </div>     
 }
 
