b9495b4d0949ce400f551a6eeb5557613d144108
hopewu0320 <gccbs01596@gmail.com>
2023-08-26 05:24:26 +0800

    9

---
+++ weather-front/src/HomePage/components/Picture.js
@@ -0,0 +1,16 @@
+import Sunny from '../../image/Sunny.jpg'
+import Rainy from '../../image/Rainy.jpg'
+const Picture = ({selectedItem}) => {
+    if(selectedItem.Guess){
+        return <div>
+            <img src={Sunny} alt="Sunny" />;
+        </div>
+    }else{
+        return <div>
+            <img src={Rainy} alt="Rainy" />;
+        </div>
+    }
+    
+}
+
+export default Picture;
