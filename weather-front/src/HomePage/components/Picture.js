5804606c50cbdf7e057be61b09d8e542289532a0
hopewu0320 <gccbs01596@gmail.com>
2023-08-26 12:25:11 +0800

    change picture

--- weather-front/src/HomePage/components/Picture.js
+++ weather-front/src/HomePage/components/Picture.js
@@ -1,14 +1,14 @@
-import Sunny from '../../image/Sunny.jpg'
-import Rainy from '../../image/Rainy.jpg'
+import Sunny2 from '../../image/Sunny2.jpg'
+import Rainy2 from '../../image/Rainy2.jpg'
 import "../index.css"
 const Picture = ({selectedItem}) => {
     if(selectedItem.Guess){
         return <div className="Picture">
-            <img src={Sunny} alt="Sunny" />
+            <img src={Sunny2} alt="Sunny2" />
         </div>
     }else{
         return <div className="Picture">
-            <img src={Rainy} alt="Rainy" />
+            <img src={Rainy2} alt="Rainy2" />
         </div>
     }
     
